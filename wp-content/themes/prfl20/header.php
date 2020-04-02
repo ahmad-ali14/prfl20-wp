9cd32a9fc903d224d9e0c411d4dfa36d8ed9171a
Ahmad Ali <aallii300300@gmail.com>
2020-04-02 05:32:43 +0100

    Update header.php

--- wp-content/themes/prfl20/header.php
+++ wp-content/themes/prfl20/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="<?php bloginfo() ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <titl><?php bloginfo('name') ?></title>
+    <title><?php bloginfo('name') ?></title>
     <?php wp_head(); ?>
     <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
    <script src="https://kit.fontawesome.com/1c03bf9151.js" crossorigin="anonymous"></script>
